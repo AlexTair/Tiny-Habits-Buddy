56ae6fd164bad6258d37e7126917aa710a9002db
AlexTair <alina.deniskova@gmail.com>
2025-07-05 04:04:56 +0300

    Create add.tsx

---
+++ app/(tabs)/add.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// This is just a placeholder for the tab navigator
+// The actual functionality is handled in the tab layout
+export default function AddScreen() {
+  return <View />;
+}
